9ca25321361385bcc4c975856ffa945e381a693b
Hallie <70453087+ha1lie@users.noreply.github.com>
2022-08-08 18:53:15 -0400

    Initial Commit

---
+++ ScreenScraps/ScreenScrapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenScrapsApp.swift
+//  ScreenScraps
+//
+//  Created by Hallie on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ScreenScrapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
